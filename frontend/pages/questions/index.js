30aa06167f7c09c00da67ef65a048b72bfcdf80d
Yashvi Mahapatra <yashvimahapatra201@gmail.comgit config --global user.email yashvimahapatra201@gmail.com>
2020-09-12 18:08:40 +0530

    added all questions page

---
+++ frontend/pages/questions/index.js
@@ -0,0 +1,9 @@
+import Layout from '../../components/layout'
+import AllQuestions from '../../components/AllQuestions'
+
+function AllQuestionsPage(){
+
+    return <AllQuestions/>
+}
+
+export default Layout(AllQuestionsPage);
