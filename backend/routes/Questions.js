766ab15f2bc351b3ca0eb45b09a49e6c37ebc58e
KAUSHIK MANDAL <64838130+Kaushik-FSD@users.noreply.github.com>
2020-09-12 15:04:31 +0530

    modified questions db

--- backend/routes/Questions.js
+++ backend/routes/Questions.js
@@ -3,35 +3,47 @@ var router = express.Router();
 var db = require("../models");
 
 //GET ROUTE(TO GET ALL QUESTION api's)
-router.get("/", function (req, res, next) {
+router.get("/", function (req, res) {
   db.Question.find()
     .then(function (questions) {
       res.json(questions);
     })
     .catch(function (err) {
-      next(err)
+      console.log(err);
     });
 });
 
 //POST ROUTE(TO POST A QUESTION)
-router.post("/", function (req, res, next) {
+router.post("/", function (req, res) {
   db.Question.create(req.body)
     .then(function (newQuestion) {
       res.json(newQuestion);
     })
     .catch(function (err) {
-      next(err);
+      console.log(err);
     });
 });
 
 //GET PERTICULAR TOPIC ROUTE
 //this route will have questions of the perticular topic (not implemented in V1.0)
-router.get("/:questionId", function (req, res, next) {
-  db.Question.findById(req.params.questionId)
+// router.get("/:questionId", function (req, res) {
+//   db.Question.findById(req.params.questionId)
+//     .populate("answers")
+//     .exec(function (err, foundQuestion) {
+//       if (err) {
+//         console.log(err);
+//       } else {
+//         res.json(foundQuestion);
+//       }
+//     });
+// });
+
+router.get("/:questionId", function (req, res) {
+  db.Question.findOne({ Q_id: req.params.questionId })
     .populate("answers")
     .exec(function (err, foundQuestion) {
       if (err) {
-        next(err)
+        console.log(err);
       } else {
         res.json(foundQuestion);
       }
@@ -40,15 +52,25 @@ router.get("/:questionId", function (req, res, next) {
 
 //ANSWERS
 
+// router.get("/answer", function (req, res) {
+//   db.Answer.find()
+//     .then(function (answer) {
+//       res.json(answer);
+//     })
+//     .catch(function (err) {
+//       console.log(err);
+//     });
+// });
+
 //TO POST A ANSWER
-router.post("/:questionId/answer", function (req, res, next) {
+router.post("/:questionId/answer", function (req, res) {
   db.Question.findById(req.params.questionId, function (err, question) {
     if (err) {
-      next(err);
+      console.log(err);
     } else {
       db.Answer.create(req.body, function (err1, newAnswer) {
         if (err1) {
-          next(err1)
+          console.log(err1);
         } else {
           res.json(newAnswer);
           question.answer.push(newAnswer._id);
