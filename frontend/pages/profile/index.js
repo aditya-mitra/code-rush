b4208ce27a9fcfeed52ee2028cf87a682d9e28f5
Yashvi Mahapatra <yashvimahapatra201@gmail.comgit config --global user.email yashvimahapatra201@gmail.com>
2020-10-08 11:32:27 +0530

    added user profile page

---
+++ frontend/pages/profile/index.js
@@ -0,0 +1,8 @@
+import Layout from '../../components/layout'
+import Profile from '../../components/Profile'
+
+function profilePage () {
+    return <Profile/>
+}
+
+export default Layout(profilePage);
