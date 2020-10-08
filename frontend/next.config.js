6023cc070f940f8b0b517782d817f2d446b268bf
aditya-mitra <ultrapokeinstinct@gmail.com>
2020-10-09 03:25:39 +0530

    update nextjs

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,6 +1,6 @@
 module.exports = {
     env: {
-        BACKEND_URL: "http://localhost:9000/api/" // change this during production
+        BACKEND_URL: "https://code-rush-backend.herokuapp.com/api/" // change this during production
         /* https://code-rush-backend.herokuapp.com/api/ - this is the production url*/
         /* http://localhost:9000/api/ - this is during development */
     },
